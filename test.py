7e9fd92a91d6c8206fa5c35b4ac94da789e0b9b7
Adam Burrough <adamjamesburrough@gmail.com>
2023-12-17 23:36:29 +0900

    stable diffusion test

--- test.py
+++ test.py
@@ -1,1 +1,33 @@
-print("opperating in VENV")
+import os 
+from PIL import Image, ImageDraw
+import cv2
+import numpy as np
+from IPython.display import HTML
+from base64 import b64encode
+
+import torch
+from torch import autocast
+from torch.nn import functional as F
+from diffusers import StableDiffusionPipeline, AutoencoderKL
+from diffusers import UNet2DConditionModel, PNDMScheduler, LMSDiscreteScheduler
+from diffusers.schedulers.scheduling_ddim import DDIMScheduler
+from transformers import CLIPTextModel, CLIPTokenizer
+from tqdm.auto import tqdm
+from huggingface_hub import notebook_login
+
+device = "cuda"
+
+notebook_login()
+
+model_id = "runwayml/stable-diffusion-v1-5"
+
+pipe = StableDiffusionPipeline.from_pretrained(
+    model_id, revision="fp16", torch_dtype=torch.float16
+)
+
+pipe = pipe.to("cuda")
+
+prompt = "a photo of an astronaut riding a horse on mars"
+image = pipe(prompt).images[0]  
+    
+image.save("astronaut_rides_horse.png")
