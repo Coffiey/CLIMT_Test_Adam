6a9682f51de4d301aa8375154e5e5450bebbe4c1
I Kayo <kayote@Is-MacBook-Air.local>
2023-12-18 16:11:18 +0900

    add image to image functionality

--- functions.py
+++ functions.py
@@ -4,6 +4,10 @@ import cv2
 import numpy as np
 from IPython.display import HTML
 from base64 import b64encode
+import requests
+import json
+
+civit_base_URL = "https://civitai.com/api/v1/"
 
 import torch
 from torch import autocast
@@ -34,3 +38,17 @@ pipe_image_to_image = AutoPipelineForImage2Image.from_pretrained(
 def create_from_text(prompt):
     image = pipe_text_to_image(prompt, num_inference_steps=10).images[0] 
     image.save(prompt.replace(" ", "_") + ".png")
+
+
+def create_from_image(prompt):
+    prompt_image = Image.open("./cow.jpeg").convert("RGB")
+    prompt_image = prompt_image.resize((768, 512))
+    image = pipe_image_to_image(prompt=prompt,image=prompt_image, num_inference_steps=5,strength=0.5, guidance_scale=7.5).images[0] 
+    image.save(prompt.replace(" ", "_") + ".png")
+
+
+def create_from_text_with_lora(prompt):
+    pipe_text_to_image.unet.load_attn_procs("aurora-style-richy-v1.safetensors", weight_name="aurora-style-richy-v1.safetensors",cross_attention_kwargs={"scale": 0.7})
+    image = pipe_text_to_image(prompt, num_inference_steps=50).images[0] 
+    image.save(prompt.replace(" ", "_") + ".png")
+    pipe_text_to_image.unload_lora_weights()
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 
-from functions import create_from_text
+from functions import create_from_text, create_from_image,create_from_text_with_lora
 
 
 app = FastAPI()
@@ -16,13 +16,21 @@ def index(prompt):
     create_from_text(image_prompt)
     return {"message" : "hello dave"}
 
-@app.get("/model/image-to-image")
-def index():
-    return {"message" : "hello Adam"}
+@app.get("/model/image-to-image/{prompt}")
+def index(prompt):
+    image_prompt = prompt.replace("_"," ")
+    create_from_image(image_prompt)
+    return {"message" : "image prompt"}
+
+@app.get("/lora/text-to-image/{prompt}")
+def index(prompt):
+    image_prompt = prompt.replace("_"," ")
+    create_from_text(image_prompt)
+    return {"message" : "loraimage"}
 
 @app.get("/*")
 def index():
-    return {"message" : "hello Adam"}
+    return {"message" : "404"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8000, reload=True)
